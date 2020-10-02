27174c939e59e445772176044ed395573ac8bc92
murtazinram <murtazinram@gmail.com>
2020-10-02 22:42:35 +0300

    feat: solved index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-
-exports.min = function min (array) {
-
-
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) return 0
+    let sort = array.sort((a, b) => a - b);
+    return sort[0]
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) return 0
+    let sort = array.sort((a, b) => b - a);
+    return sort[0]
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) return 0
+    let summa = 0
+    for (const item of array) summa += item
+    return summa / array.length
 }
